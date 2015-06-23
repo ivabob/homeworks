c001bc6a2499368870233e17c727d39d896518a4
ivabob <ivabob@users.noreply.github.com>
2015-06-23 14:45:56 +0300

    Create task2.php

---
+++ task2.php
@@ -0,0 +1,16 @@
+<?php
+$a=8;
+$b=1.57;
+$c="Hello, it's me!";
+$d=true;
+$e=false;
+var_dump($a);
+print "</br>";
+var_dump($b);
+print "</br>";
+print $c;
+print "</br>";
+print $d;
+print "<br>";
+var_dump($e);
+?>
