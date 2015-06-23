705f181ec597441e97e3ab7ef68c1d97c6f56b1e
ivabob <ivabob@users.noreply.github.com>
2015-06-23 14:48:26 +0300

    Create task6.php

---
+++ task6.php
@@ -0,0 +1,8 @@
+<?php
+$films=array('Vreme razdelno' => 'Vasil Mihailov', 'Pritty woman' => 'Julia Roberts', 'The Godfader' => 'Al Pacino', 'Na vseki kilometar' => 'Stefan Danailov', 'Stolichani v poveche' => 'Silvia Lulcheva', );
+print 'The main actor in the move "Pritty woman" is:';
+print $films['Pritty woman'];
+print '</br>';
+print 'The main actor in the move "Vreme razdelno" is:';
+print $films['Vreme razdelno'];
+?>
