d10bc64fc7d4c9216f41932956f58b54201455c0
ivabob <ivabob@users.noreply.github.com>
2015-07-04 15:29:54 +0300

    Update task3.php

--- task3.php
+++ task3.php
@@ -1,10 +1,24 @@
+<html>
+     <head>
+     <title>learn</title>
+     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+     </head>
+  <body>
+       <form method="post" action="learn3.php">
+       Въведи първо число: <input type="text" name="num1" value="num1">
+       Въведи второ число: <input type="text" name="num2" value="num2">
+       <input type="submit" name="submit" value="submit">
+       </form>  
 <?php
-$a=7;
-$b=20;
-$c=$a*2 + $b*2;
-$s=$a*$b;
-print $c;
-print "</br>";
-print $s;
-print "</br>";
+$a = ($_POST['num1']);
+$b = ($_POST['num2']);
+$c = $a * $b;
+   if (($c%2) == 0)  {
+	echo "произведението е четно число";
+   }
+    else {
+    	echo "произведението е нечетно число";
+    }
 ?>
+</body>
+</html>
