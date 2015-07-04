359d774cf00c0cfe3aaaf6a0b3278d66a09e9785
ivabob <ivabob@users.noreply.github.com>
2015-07-04 16:54:13 +0300

    Update task25.php

--- task25.php
+++ task25.php
@@ -1,6 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>cyrcle</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+     <h3>Въведете координати за произволна точка:</h3>
+     <form action="" method="post">
+     	<input type="text" name="x" value="x">
+     	<input type="text" name="y" value="y">
+     	<input type="submit" name="submit" value="submit">
+     </form>
 <?php
-$x=4;
-$y=1;
-if ($x > -2 && $x < 2 && $y > -2 && $y < 2){echo "Tochkata se namira v kraga";}
-   else {echo "Tochkata e van ot kraga";}
-?>
+$x0 = 0;
+$y0 = 0;
+$r = 2;
+$x = (!empty($_POST['x']));
+$y = (!empty($_POST['y']));
+$s = sqrt(($x0 - $x) * ($x0 - $x) + ($y0 - $y) * ($y0 - $y));
+   if ($s < $r){
+   		echo "Точката е в кръга";
+   	} 
+   	elseif ($s == $r){
+   		echo "Точката е на кръга";
+   	} 
+   	else {
+   	 echo "Точката е извън кръга";
+   	}
+   
+  ?>
+</body>
+</html>
