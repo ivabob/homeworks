c6bf80761756d5463fab17e67a1c4546f183c2e3
ivabob <ivabob@users.noreply.github.com>
2015-07-11 16:47:11 +0300

    Create task2.php

---
+++ homework7/task2.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<title>position</title>
+</head>
+<body>
+<div class="row">
+	<div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="walter.jpg" alt="Walter O'Brien" class="img-circle">
+      <div class="caption">
+        <h3>Walter O'Brien</h3>
+        <p>Walter O'Brien was born to Maurice and Anne O'Brien in 1975 in Clonroche, Ireland.The second of five children, he grew up on a farm.O'Brien attended St. Patrick's National School in Clonroche for elementary education, until his family moved to Rosshaven, when he was 13 years old. There he attended St. Mary's Christian Brothers School (CBS) in Enniscorthy.</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="quin.jpg" alt="Happy Quin" class="img-circle">
+      <div class="caption">
+        <h3>Happy Quinn</h3>
+        <p>Happy has a violent interior and exterior. She portrays anger so she can turn any pain into that. She isn't just physically violent, though, she is also verbally violent. Within even the pilot, she uses threatening quips to, possibly, defuse her annoyance with their fear. For a girl who lived within the foster system, she did have to defend herself and that mostly is where she developed her violent tendencies. Not to break or cry, probably to keep herself from being judged, just like Walter had found her (except in a different way). She can be open to people similar to her, such as Ralph, but she also acts that way to him to make sure he doesn't end up like she did; in a foster home being misunderstood. She is very fearful to trusting others and getting hurt. If anything, she is a significant character, but harder to control.</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="toby.jpg" alt="Toby" class="img-circle">
+      <div class="caption">
+        <h3>Toby</h3>
+        <p>Toby is a popular male name in many English speaking countries. The name is from the Middle English vernacular form of Tobias. Tobias itself is the Greek transliteration of the Hebrew Toviah, which translates to Good is Yahweh. Yahweh is the name of the Jewish national god. Tobit is also an alternate form of Tobias.</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
