776d85f635c72b09926a21b814233dedd90cf808
ivabob <ivabob@users.noreply.github.com>
2015-07-11 18:08:28 +0300

    Create zad2.php

---
+++ homework7/zad2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<title>position</title>
+</head>
+<body>
+<ul class="list-group">
+  <li class="list-group-item list-group-item-success">
+    <h3>Furious 7</h3>
+    <span> After defeating international terrorist Owen Shaw, Dominic Toretto (Vin Diesel), Brian O'Conner (Paul Walker) and the rest of the crew have separated to return to more normal lives. However, Deckard Shaw (Jason Statham), Owen's older brother, is thirsty for revenge. A slick government agent offers to help Dom and company take care of Shaw in exchange for their help in rescuing a kidnapped computer hacker who has developed a powerful surveillance program.
+Initial release: March 26, 2015 (Croatia)
+Director: James Wan
+Running time: 2h 20m
+Film series: The Fast and the Furious
+Producers: Vin Diesel, Neal H. Moritz, Michael Fottrell
+  </span>
+  </li>
+  <li class="list-group-item list-group-item-warning">
+    <h3> Pretty Woman</h3>
+    <span>In this modern update on Cinderella, a prostitute and a wealthy businessman fall hard for one another, forming an unlikely pair. While on a business trip in L.A., Edward (Richard Gere), who makes a living buying and breaking up companies, picks up a hooker, Vivian (Julia Roberts), on a lark. After E… More
+Initial release: March 23, 1990 (Canada)
+Director: Garry Marshall
+Running time: 2h 5m
+Featured song: Kiss
+Music composed by: James Newton Howard</span>
+  </li>
+  <li class="list-group-item list-group-item-success">
+    <h3>The Godfather</h3>
+    <span>Widely regarded as one of the greatest films of all time, this mob drama, based on Mario Puzo's novel of the same name, focuses on the powerful Italian-American crime family of Don Vito Corleone (Marlon Brando). When the don's youngest son, Michael (Al Pacino), reluctantly joins the Mafia, he become… More
+Initial release: March 15, 1972 (New York City)
+Director: Francis Ford Coppola
+Film series: The Godfather
+Music composed by: Nino Rota
+Featured songs: Speak Softly Love, I Have But One Heart</span>
+  </li>
+  <li class="list-group-item list-group-item-warning">
+  <h3>Dance with Me</h3>
+  <span>Young Cuban Rafael just buried his mother, and comes to Houston to meet his father John for the first time. The difficult part is that John doesn't know he is Rafael's father. John runs a ... See full summary »
+
+Director: Randa Haines
+Writer: Daryl Matthews
+Stars: Vanessa Williams, Chayanne, Kris Kristofferson </span>
+  </li>
+</ul>
+</body>
+</html>
