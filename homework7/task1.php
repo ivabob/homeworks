f8a6c99bf0949d0c6e27d1ea870fa1cc5d2e2ce6
ivabob <ivabob@users.noreply.github.com>
2015-07-11 16:42:08 +0300

    Create task1.php

---
+++ homework7/task1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<title>position</title>
+</head>
+<body>
+<img src="pic1.jpg" alt="picture1" class="img-circle">
+<p></p>
+<img src="pic2.jpg" alt="picture2" class="img-circle">
+</body>
+</html>
