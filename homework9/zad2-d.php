887a0cb02d298a72c7920bec56fd7622b6dc5f7d
ivabob <ivabob@users.noreply.github.com>
2015-07-25 19:41:29 +0300

    Update zad2-d.php

--- homework9/zad2-d.php
+++ homework9/zad2-d.php
@@ -1,6 +1,8 @@
 <?php
+$dlop_id = $_GET['id'];
+
 $conn = mysqli_connect('localhost', 'root', '', 'vratsa_dlopia');
-$deldlop="UPDATE `dlops` SET date_deleted = now() WHERE id = 6";
+$deldlop="UPDATE `dlops` SET date_deleted = now() WHERE id = $dlop_id";
 $result = mysqli_query($conn, $deldlop);
 echo "<a href='zad2-a.php' class='btn btn-link' type='button'> Back </a>";
 ?>
